daa72543817706aee3b1535fbebaab7b85666597
Shreyans jain <shreyansjain1603@gmail.com>
2023-10-10 20:53:36 +0530

    commit

--- app/App.js
+++ app/App.js
@@ -1,9 +1,16 @@
 import { StatusBar } from "expo-status-bar";
+import { createContext, useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import StackNavigator from "./StackNavigator";
 
+export const UserContext = createContext(null);
+
 export default function App() {
-  return <StackNavigator />;
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <StackNavigator />
+    </UserContext.Provider>
+  );
 }
 
 const styles = StyleSheet.create({});
