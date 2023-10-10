91a11811b03c3311fda428361948ef6bbfb1f769
Shreyans jain <shreyansjain1603@gmail.com>
2023-10-10 20:55:15 +0530

    commit

--- app/App.js
+++ app/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import StackNavigator from "./StackNavigator";
 
 export const UserContext = createContext(null);
 
 export default function App() {
+  const [user, setUser] = useState({});
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <StackNavigator />
