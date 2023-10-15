391c723ee29ab9abfc0bf26463acd274a19342a1
Varun <varun.jajoo18@gmail.com>
2023-10-15 10:47:15 +0530

    commit

--- app/Screens/Main/Main.js
+++ app/Screens/Main/Main.js
@@ -4,6 +4,7 @@ import { useUserContext } from "../../UserContext";
 import { useNavigation } from "@react-navigation/native";
 import * as Location from 'expo-location';
 import { useLocationContext } from "../../LocationContext";
+import * as Speech from 'expo-speech'; // Import the Speech module
 import Header from "../../components/Header";
 
 const Main = () => {
@@ -25,13 +26,14 @@ const Main = () => {
   }, []);
 
   const cardData = [
-    { id: 1, title: "Exercise", image: require("../../assets/exercise.png"), backgroundColor:"#02cfee",to:''},
-    { id: 2, title: "Medicine", image: require("../../assets/medicine.png"),backgroundColor:"#f67ea9",to:'Med'  },
-    { id: 3, title: "Problems", image: require("../../assets/problem.png"),backgroundColor:"#53dab8",to:'problems'  },
-    { id: 4, title: "Family", image: require("../../assets/family.png"),backgroundColor:"#f0a540",to:'family'  },
-    { id: 5, title: "Parks Nearby", image: require("../../assets/forest.png"),backgroundColor:"#02cfee",to:'Map'  },
-    { id: 6, title: "Doctor", image: require("../../assets/doctor.png"),backgroundColor:"#02cfee",to:''  },
-  ]
+    { id: 1, title: "Exercise", image: require("../../assets/exercise.png"), backgroundColor: "#02cfee", to: '' },
+    { id: 2, title: "Medicine", image: require("../../assets/medicine.png"), backgroundColor: "#f67ea9", to: 'Med' },
+    { id: 3, title: "Problems", image: require("../../assets/problem.png"), backgroundColor: "#53dab8", to: 'problems' },
+    { id: 4, title: "Family", image: require("../../assets/family.png"), backgroundColor: "#f0a540", to: 'family' },
+    { id: 5, title: "Parks Nearby", image: require("../../assets/forest.png"), backgroundColor: "#02cfee", to: 'Map' },
+    { id: 6, title: "Doctor", image: require("../../assets/doctor.png"), backgroundColor: "#02cfee", to: 'Doc' },
+  ];
+
   const navigation = useNavigation();
 
   const { user } = useUserContext();
@@ -47,21 +49,19 @@ const Main = () => {
  }
   return (
     <SafeAreaView style={{ paddingTop: Platform.OS === 'android' ? 40 : 0, justifyContent: "center", alignItems: "center" }}>
-      <ScrollView>
-        <View>
-          <Header home={true} />
-          {/* <ImageBackground source={require("../../assets/map.png")} style={styles.topCard}>
-            <View style={styles.overlay} />
-            <View style={{ display: "flex", justifyContent: "center", alignItems: "left" }}>
-              <TouchableOpacity>
-                <Text style={{ color: "white", fontWeight: "bold", fontSize: 60 }}>Nearby Parks <Image source={require("../../assets/map-location_3466163.png")} style={{ width: 40, height: 40, marginVertical: 10 }} /> </Text>
-                <Text style={{ color: "white", fontWeight: 500, fontSize: 20, marginTop: 10 }}>Discover Public Parks</Text>
-                <Text style={{ color: "white", fontSize: 17, marginTop: 10 }}>
-                  Enjoy a peaceful walk amidst nature's beauty.
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </ImageBackground> */}
+      <ScrollView style={{ display: "flex" }}>
+        <View style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          {/* <Text className="mt-7 ml-6 font-normal text-[32px] mb-0 py-0"> */}
+          <View style={{display:"flex",flexDirection:"row",justifyContent:"space-between",alignItems:"center",marginBottom:20}}>
+          <Text style={{padding:25,backgroundColor:"#53dab8",fontSize:25,marginRight:10,borderRadius:30,fontWeight:"bold"}}>
+            <Image source={require('../../assets/wave.png')} style={{height:29,width:29,paddingRight:20}}/>
+          <Text>{"  "}</Text>
+            Welcome, {user?.data?.first_name ? user?.data?.first_name + "!" : "User!"}
+          </Text>
+          <Pressable style={{height:90,width:90,backgroundColor:"#53dab8",display:"flex",justifyContent:"center",alignItems:"center",borderRadius:30}} onPress={HandleClick}>
+          <Image source={require('../../assets/avatar/dadaji-removebg-preview.png')} style={{height:60,width:60,borderRadius:50,backgroundColor:"#1c455b"}}/>
+          </Pressable>
+          </View>
           <View style={styles.rockbottom}>
             <View
               style={{
